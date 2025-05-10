cc172ce321f396fd6be884c3ba9bd24ce8619a8e
Ghostboi69sad <mohamedaminem240@gmail.com>
2025-05-10 06:22:33 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // ...
+  base: '/',
   publicDir: 'public',
-  // ...
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 }) 
